214d54dab3c5f408e632b5e16ea5a5e2bbd337d8
Saurabh Kadtan <saurabhkadtan@gmail.com>
2025-12-16 23:52:43 +0530

    Coffee Shop Website structure and styling

---
+++ script.js
@@ -0,0 +1,48 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+
+
+
+menuOpenButton.addEventListener("click", () => {
+    //Toggle mobile-menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => {
+    menuOpenButton.click();
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCurssor:true,
+    spaceBetween:25,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0:{
+            slidesPerView:1
+        },
+        768:{
+            slidesPerView:2
+        },
+        1024:{
+            slidesPerView:3
+        },
+    }
+  });
